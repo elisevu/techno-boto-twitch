4c8f420ca263abcbebfc107c291944174a541f69
elisevu <elise.k.vu@gmail.com>
2022-06-06 20:27:56 -0400

    include example for constants.js secrets

---
+++ src/constants_example.js
@@ -0,0 +1,7 @@
+export const CHANNEL_NAME = '#yourChannelName';
+export const OAUTH_TOKEN = 'oauth:yourToken';
+export const BOT_USERNAME = 'yourChannelName';
+export const BLOCKED_WORDS = [
+    'cats',
+    'dogs'
+]
